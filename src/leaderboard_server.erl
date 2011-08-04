646739214eb73054be8943d74733847651d47046
Armando Di Cianno <adicianno@agoragames.com>
2011-08-04 18:36:22 -0400

    Added delete, rank, remove, total

--- src/leaderboard_server.erl
+++ src/leaderboard_server.erl
@@ -2,72 +2,67 @@
 -behaviour(gen_server).
 -define(SERVER, ?MODULE).
 
-%% ------------------------------------------------------------------
-%% API Function Exports
-%% ------------------------------------------------------------------
-
+%%% exports
 -export([start_link/0, create/2]).
-
-%% ------------------------------------------------------------------
-%% gen_server Function Exports
-%% ------------------------------------------------------------------
-
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
-%% ------------------------------------------------------------------
-%% records
-%% ------------------------------------------------------------------
+%%% records
+-record(state, {host,
+		port,
+		client}).
 
-%-record(state, {socket,
-%                port,
-%                acceptor}).
-
-%% ------------------------------------------------------------------
-%% API Function Definitions
-%% ------------------------------------------------------------------
-
+%%% definitions
 start_link() ->
     gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
 create(ServerPid, Pid) ->
     gen_server:cast(ServerPid, {create, Pid}).
 
-%% ------------------------------------------------------------------
-%% gen_server Function Definitions
-%% ------------------------------------------------------------------
+%%% gen_server defintions
+init(_Args) ->
+    %[RedisHost, RedisPort] = Args,
+    RedisHost = "127.0.0.1",
+    RedisPort = 6379,
+    process_flag(trap_exit, true),
+    case erldis:connect(RedisHost, RedisPort) of
+	{ok, Client} ->
+	    error_logger:info_msg("Connected to redis client<~p>~n", [Client]),
+	    {ok, #state{host = RedisHost, port = RedisPort, client = Client}};
+	X ->
+	    error_logger:error_msg("Could not connect to redis: ~p~n", [X]),
+	    {stop, X}
+    end.
 
-init(Args) ->
-    process_flag(trap_exit, true).
+handle_call({members_total, Leaderboard}, _From, #state{client = Client} = State) ->
+    {reply, members_total(Client, Leaderboard), State};
 
 handle_call(_Request, _From, State) ->
-  {noreply, ok, State}.
+    {noreply, ok, State}.
 
-%handle_cast({create, _Pid}, #state{socket = Socket} = State) ->
-%    New_pid = vdpms_server_socket:start_link(self(), Socket, State#state.port),
-%    {noreply, State#state{acceptor=New_pid}};
+handle_cast({leaderboard_delete, Leaderboard}, #state{client = Client} = State) ->
+    {noreply, erldis:del(Client, Leaderboard), State};
+
+handle_cast({member_rank, Member, Score}, #state{client = Client} = State) ->
+    {noreply, erldis:zadd(Client, Score, Member), State};
+
+handle_cast({member_remove, Member}, #state{client = Client} = State) ->
+    {noreply, erldis:zrem(Client, Member), State};
 
 handle_cast(_Msg, State) ->
-  {noreply, State}.
-
-%% %% The current acceptor has died, log it out of interest, and move along
-%handle_info({'EXIT', Pid, Abnormal}, #state{acceptor=Pid} = State) ->
-%    error_logger:warning_msg("server_socket handler PID(~p) exited with (~p)", [Pid, Abnormal]),
-%    {noreply, State};
-
-%% Specialize normal exit
-%handle_info({'EXIT', Pid, normal}, #state{acceptor=Pid} = State) ->
-%    {noreply, State};
+    {noreply, State}.
 
 handle_info(_Info, State) ->
-  {noreply, State}.
+    {noreply, State}.
 
 terminate(_Reason, _State) ->
-  ok.
+    ok.
 
 code_change(_OldVsn, State, _Extra) ->
-  {ok, State}.
+    {ok, State}.
 
 %% ------------------------------------------------------------------
 %% Internal Function Definitions
 %% ------------------------------------------------------------------
 
+members_total(Client, Leaderboard) ->
+    erldis:zcard(Client, Leaderboard).
